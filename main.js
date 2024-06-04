aa7977e4d9ae1eef47f0f4cbb03b3e1af9407b86
Michele Ferrer <michele.e.ferrer@gmail.com>
2024-06-04 07:47:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+input = a=2, b=3
+
+function add (a,b){
+    return a + b;
+}
+
+function helloworld(){
+    return "hello, world!"
+}
+
+arr = [1, 2, 3, 4]
+arr = [0, -1, 1]
+arr = []
+
+function sumArray(arr) {
+    let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+    
+        return sum;
+    }
